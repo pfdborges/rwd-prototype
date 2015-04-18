d3be7f659e7b16f8ccd0191923d3a6d498b8b054
Pedro Borges <pedrodavidborges@gmail.com>
2015-04-18 19:51:58 +0100

    init

---
+++ assets/js/scripts.js
@@ -0,0 +1,15 @@
+jQuery(document).ready(function($){
+
+	// Modal
+	Ink.requireModules( ['Ink.Dom.Selector_1','Ink.UI.Modal_1'], function( Selector, Modal ){
+        var modalElement = Ink.s('#desktopModal');
+        var modalObj = new Modal( modalElement );
+    });
+
+	// Animations
+	$(".rwd-header").addClass("animated fadeInDown");
+	$(".project-name").addClass("animated fadeInDown delay");
+	$(".credits").addClass("animated fadeInUp");
+	$(".sprite.brand-logo").addClass("animated fadeIn");
+
+});
