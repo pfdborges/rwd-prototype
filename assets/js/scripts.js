fd429e880e7bcf38c06a85d733e5471b6aea5d96
Pedro Borges <pedrodavidborges@gmail.com>
2015-04-20 11:40:43 +0100

    adding project description and gitignore file

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -9,6 +9,7 @@ jQuery(document).ready(function($){
 	// Animations
 	$(".rwd-header").addClass("animated fadeInDown");
 	$(".project-name").addClass("animated fadeInDown delay");
+	$(".project-description").addClass("animated fadeInDown delay");
 	$(".credits").addClass("animated fadeInUp");
 	$(".sprite.brand-logo").addClass("animated fadeIn");
 
